139d6c5c5c56012f481e7cbac77c365350cadebd
annagerchanovsky <57304421+annagerchanovsky@users.noreply.github.com>
2022-07-26 22:47:17 -0700

    moodboard

---
+++ moodboard/silly_script.py
@@ -0,0 +1,10 @@
+# for i in range(3, 106):
+#     print(f"<img src=\"moodboard_pics/JPEG image {i}.jpeg\" height=\"200\">")
+
+import os
+directory = 'moodboard_pics'
+ 
+# iterate over files in
+# that directory
+for filename in os.listdir(directory):
+    print(f"<img src=\"moodboard_pics/{filename}\" height=\"200\">")

eceed82ab01d3883b9387f9574345141c7c45a4f
annagerchanovsky <57304421+annagerchanovsky@users.noreply.github.com>
2022-07-27 00:13:47 -0700

    moodboard format2

--- moodboard/silly_script.py
+++ moodboard/silly_script.py
@@ -7,4 +7,5 @@ directory = 'moodboard_pics'
 # iterate over files in
 # that directory
 for filename in os.listdir(directory):
-    print(f"<img src=\"moodboard_pics/{filename}\" style=\"border:0px;margin:200px\" width=\"450\"><br>")
+    if filename != ".DS_Store":
+        print(f"<img src=\"moodboard_pics/{filename}\" style=\"border:0px;margin:200px\" width=\"450\"><br>")

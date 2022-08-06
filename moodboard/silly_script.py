002286d3945d3ab343ba7ca0ebd87876d3e2e9ec
annagerchanovsky <57304421+annagerchanovsky@users.noreply.github.com>
2022-08-06 10:34:29 -0700

    edit camera roll

--- moodboard/silly_script.py
+++ moodboard/silly_script.py
@@ -8,4 +8,5 @@ directory = 'moodboard_pics'
 # that directory
 for filename in os.listdir(directory):
     if filename != ".DS_Store":
-        print(f"<img src=\"moodboard_pics/{filename}\" style=\"border:0px;margin:200px\" width=\"450\"><br>")
+        # print(f"<img src=\"moodboard_pics/{filename}\" style=\"border:0px;margin:200px\" width=\"450\"><br>")
+        print(f"<img src=\"moodboard_pics/{filename}\" height=\"450\">")
